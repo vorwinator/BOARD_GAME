464c75653bb7c6796d981f57dfce39eb71cf776e
vorwinator <vorwi111@gmail.com>
2023-01-21 10:13:37 +0100

    square board and comments

--- board.php
+++ board.php
@@ -1,29 +1,32 @@
 <?php
-
+/**
+ * @var $board array - contains all of current game board data
+ */
 class board extends MainController{
 
     public array $board;
 
-    function generateNewBoard($tr = 10, $td = 10){
-        if($tr < 3) $tr = 3;
+    /**
+     * @param $square int - contains number of rows and columns
+     */
+    function generateNewBoard($square = 10){
+        if($square < 3) $square = 3;
         $boardCellId = 0;
         $this->board['html'] = '<table style="border: black solid 3px; height:500px; width:500px">';
-        for($i = 0; $i < $tr; $i++){
+        for($i = 0; $i < $square; $i++){
             $this->board['html'] .= '<tr style="border: black solid 3px;">';
-            for($j = 0; $j < $td; $j++){
-                if($i == 0 || $i == $tr-1){
+            for($j = 0; $j < $square; $j++){
+                if($i == 0 || $i == $square-1){//top and boottom
                     $this->board['html'] .= '<td id="Cell_'. $boardCellId .'" style="border: black solid 3px;"></td>';
                     $boardCellId++;
                 }
-                elseif($i != 0 && $i != $tr-1 && ($j == 0 || $j == 1)){
+                elseif($i != 0 && $i != $square-1 && ($j == 0 || $j == 1)){//left and right
                     $this->board['html'] .= '<td id="Cell_'. $boardCellId .'" style="border: black solid 3px;"></td>';
                     $boardCellId++;
-                }
-                elseif($i==1 && $j==0){
-                    $this->board['html'] .= '<td id="Cell_center" colspan="'. $td-2 .'" style="border: black solid 3px;"></td>';
-                }
-                else{
-                    $this->board['html'] .= '<td></td>';
+
+                    if($i==1 && $j==0){//center
+                        $this->board['html'] .= '<td id="Cell_center" colspan="'. $square-2 .'" rowspan="'. $square-2 .'" style="border: black solid 3px;"></td>';
+                    }
                 }
             }
             $this->board['html'] .= "</tr>";
